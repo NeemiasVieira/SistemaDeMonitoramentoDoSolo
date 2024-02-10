3b8ee9630efa47d411115dd32a0f0ea3846f9d7b
Neemias Vieira <neemias.vieira@live.com>
2024-02-10 19:37:51 -0300

    Implementações de segurança

--- src/services/API/PlantsService.ts
+++ src/services/API/PlantsService.ts
@@ -12,12 +12,13 @@ export class PlantsService {
     }
 
     async getPlantsByOwnerID(idDono: string){
-        const Query = `query GetPlantasByDonoId($idDono: String!) {        
-          getPlantasByDonoId(idDono: $idDono) { id nome especie dataDaPlantacao }}`;
+        const token = `Bearer ${localStorage.getItem("token")}`;
+        const query = `query GetPlantasByDonoId {        
+          getPlantasByDonoId { id nome especie dataDaPlantacao }}`;
 
-          const options = {query: Query, variables: { idDono: idDono }};
-
-        await SMS_API.post('', options)
+          const options = { headers: {  Authorization: token, }};
+      
+        await SMS_API.post('', {query}, options)
         .then(response => {
             if(response.data && response.data.data && response.data.data.getPlantasByDonoId) this.setResponse(response.data.data.getPlantasByDonoId);
             else if (response.data && response.data.errors) this.setError(response.data.errors[0].message);
@@ -27,12 +28,14 @@ export class PlantsService {
 
     async getRelatorioDeSaude(idPlanta: string){
 
-        const Query = `query GetSaudeByPlantId($idPlanta: String!) {
+        const token = `Bearer ${localStorage.getItem("token")}`
+        const options = { headers: { Authorization: token }}
+        const variables = { idPlanta };
+        const query = `query GetSaudeByPlantId($idPlanta: String!) {
             getSaudeByPlantId(idPlanta: $idPlanta) { nitrogenio fosforo potassio umidade temperatura
-                pH estadoGeral ultimaAtualizacao alertas }}`;
-        const options = {query: Query, variables: { idPlanta }}           
+                pH estadoGeral ultimaAtualizacao alertas }}`;                
 
-        await SMS_API.post('', options).then((response) => {
+        await SMS_API.post('', {query, variables}, options).then((response) => {
             if(response.data && response.data.data && response.data.data.getSaudeByPlantId) this.setResponse(response.data.data.getSaudeByPlantId);
             else if (response.data && response.data.errors) this.setError(response.data.errors[0].message);
         })
@@ -40,11 +43,13 @@ export class PlantsService {
     }
 
     async getUltimoRegistro(idPlanta: string){
-        const Query = `query GetLastRecordByPlant($idPlanta: String!) {
-            getLastRecordByPlant(idPlanta: $idPlanta) { nitrogenio fosforo potassio umidade temperatura pH dataDeRegistro}}`;
-        const options = {query: Query, variables: {idPlanta}}    
+        const token = `Bearer ${localStorage.getItem("token")}`;
+        const options = { headers: {  Authorization: token, }};
+        const variables = { idPlanta }
+        const query = `query GetLastRecordByPlant($idPlanta: String!) {
+            getLastRecordByPlant(idPlanta: $idPlanta) { nitrogenio fosforo potassio umidade temperatura pH dataDeRegistro}}`; 
 
-        await SMS_API.post('', options).then((response) => {
+        await SMS_API.post('', {query, variables}, options).then((response) => {
             if(response.data && response.data.data && response.data.data.getLastRecordByPlant) this.setResponse(response.data.data.getLastRecordByPlant);
             else if (response.data && response.data.errors) this.setError(response.data.errors[0].message);
         })
--- src/services/API/RecordsService.ts
+++ src/services/API/RecordsService.ts
@@ -13,16 +13,20 @@ export class RecordsService{
 
     async getRecordsByPlantID(idPlanta: string, intervaloDeDias?: any, intervaloDeBusca?: any){
 
+        const token = `Bearer ${localStorage.getItem("token")}`;
+
         intervaloDeBusca = intervaloDeBusca === "Selecione" ? null : Number(intervaloDeBusca);
         intervaloDeDias = intervaloDeDias === "Selecione" ? null : Number(intervaloDeDias);
 
 
-        const Query = `query GetAllRecordsByPlant($idPlanta: String!, $intervaloDeDias: Float, $intervaloDeBusca: Float ) {
+        const query = `query GetAllRecordsByPlant($idPlanta: String!, $intervaloDeDias: Float, $intervaloDeBusca: Float ) {
             getAllRecordsByPlant( idPlanta: $idPlanta, intervaloDeDias: $intervaloDeDias, intervaloDeBusca: $intervaloDeBusca )   
             { nitrogenio fosforo potassio umidade temperatura pH dataDeRegistro } }`
         
-        const options = {query: Query, variables: {idPlanta, intervaloDeDias, intervaloDeBusca}}
-        await SMS_API.post('', options).then((response) => {
+        const options = { headers: { Authorization: token }}
+        const variables = {idPlanta, intervaloDeDias, intervaloDeBusca};
+        
+        await SMS_API.post('', {query, variables},options).then((response) => {
             if(response.data && response.data.data && response.data.data.getAllRecordsByPlant) this.setResponse(response.data.data.getAllRecordsByPlant);
             else if (response.data && response.data.errors) this.setError(response.data.errors[0].message);
         })
