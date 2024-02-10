a242f5ee362207900d578d8c7eb8ef9358fa0a18
Neemias Vieira <neemias.vieira@live.com>
2024-02-10 00:30:04 -0300

    fix: API URL

--- src/services/API/sms-api.ts
+++ src/services/API/sms-api.ts
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-// const SMS_API = axios.create({
-//     baseURL: 'https://sms-api-git-main-neemiasvieira.vercel.app/',
-//     timeout: 10000,
-//   });
 const SMS_API = axios.create({
-    baseURL: 'http://localhost:3333/',
+    baseURL: 'https://sms-api-git-main-neemiasvieira.vercel.app/',
+    // baseURL: 'http://localhost:3333/',
     timeout: 10000,
   });
 
