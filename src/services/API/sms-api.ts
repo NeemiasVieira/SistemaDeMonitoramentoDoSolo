6f2b2cd0ac9fa8fa87327adc99dc5b36c6f64ea7
Neemias Vieira <neemias.fernandes@nutrien.com>
2024-04-08 13:55:53 -0300

    api-test

--- src/services/API/sms-api.ts
+++ src/services/API/sms-api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const SMS_API = axios.create({
-    baseURL: 'https://sms-api-oe8t.onrender.com/',
-    // baseURL: "http://3.95.189.148:3333/graphql",
+    // baseURL: 'https://sms-api-oe8t.onrender.com/',
+    baseURL: "http://3.95.189.148:3333/graphql",
     // baseURL: 'https://sms-api-git-main-neemiasvieira.vercel.app/',
     // baseURL: 'http://localhost:3333/graphql',
     timeout: 90000,
